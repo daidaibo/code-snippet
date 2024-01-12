abb87ee21bf2c5c71439c0a7dccf071c7769f4c3
波波 <34775160+daidaibo@users.noreply.github.com>
2024-01-12 17:24:43 +0800

    Delete utils-工具函数.js

--- utils-工具函数.js
+++
@@ -1,7 +0,0 @@
-// flat 扁平化数组
-function flat(arr) {
-  const isDeep = arr.some(item => item instanceof Array)
-  if (!isDeep) return arr
-  const res = Array.prototype.concat.apply([], arr)
-  return flat(res)
-}
