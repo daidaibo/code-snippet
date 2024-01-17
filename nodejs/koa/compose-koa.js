b6ab4175fb12505c67c7ce4993593fe899e4d416
daibo <287314864@qq.com>
2024-01-17 15:19:43 +0800

    compose

---
+++ nodejs/koa/compose-koa.js
@@ -0,0 +1,43 @@
+async function fn1(next) {
+  console.log('fn1')
+  await next()
+  console.log('End fn1')
+}
+
+async function fn2(next) {
+  console.log('fn2')
+  await delay()
+  await next()
+  console.log('End fn2')
+}
+
+async function fn3(next) {
+  console.log('fn3')
+  await next()
+  console.log('End fn3')
+}
+
+function delay() {
+  return new Promise((reslove) => {
+    setTimeout(() => {
+      console.log('~~delay~~')
+      reslove()
+    }, 2000)
+  })
+}
+
+function compose(middlewares) {
+  return function () {
+    return dispatch(0)
+    function dispatch(i) {
+      let fn = middlewares[i]
+      return Promise.resolve(
+        fn && fn(function next() {
+          return dispatch(i + 1)
+        })
+      )
+    }
+  }
+}
+
+compose([fn1, fn2, fn3])()
