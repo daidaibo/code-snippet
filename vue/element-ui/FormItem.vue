40b50a76d92343b3c3fd7f2daa447ae1fbd8e0cd
daibo <287314864@qq.com>
2024-07-09 22:22:03 +0800

    el-form

---
+++ vue/element-ui/FormItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <slot></slot>
+    <p v-if="error" style="color: red;">
+      {{ error }}
+    </p>
+  </div>
+</template>
+
+<script>
+import Schema from 'async-validator'
+
+export default {
+  inject: ['form'],
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    prop: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      error: ''
+    }
+  },
+  mounted() {
+    this.$on('validate', this.validate)
+  },
+  methods: {
+    validate(cb = () => { }) {
+
+      const rules = this.form.rules[this.prop]
+
+      const value = this.form.model[this.prop]
+
+      const descriptor = { [this.prop]: rules }
+
+      const schema = new Schema(descriptor)
+
+      schema.validate({ [this.prop]: value }, errors => {
+        if (errors) {
+          this.error = errors[0].message
+        } else {
+          this.error = ''
+        }
+
+        cb(this.error)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ vue/element-ui/Input.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input :type="type" :value="value" @blur="onBlur" @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    }
+  },
+  methods: {
+    onBlur() {
+      this.$dispatch('validate')
+    },
+    onInput(e) {
+      this.$emit('input', e.target.value)
+      this.$dispatch('validate')
+    }
+  }
+}
+</script>
+
+<style scoped></style>
