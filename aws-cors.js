7c4d8e8a8bcf08a8141d2d0928827af7f5127bf5
daibo <287314864@qq.com>
2025-03-17 14:21:17 +0800

    亚马逊 CDN 跨域资源共享

---
+++ aws-cors.js
@@ -0,0 +1,67 @@
+// Amazon S3 存储桶
+[
+  {
+    "AllowedHeaders": [
+      "Origin"
+    ],
+    "AllowedMethods": [
+      "GET",
+      "HEAD"
+    ],
+    "AllowedOrigins": [
+      "https://wawotv.com",
+      "https://www.wawotv.com",
+      "https://admin.dreamemovie.com",
+      "https://app-admin.dreamemovie.com",
+      "https://admin.wawotv.com",
+      "https://app-admin.wawotv.com",
+      "http://localhost:3000",
+      "http://172.28.51.18:3000",
+      "http://127.0.0.1:8080"
+    ],
+    "ExposeHeaders": [],
+    "MaxAgeSeconds": 60
+  }
+]
+
+// Cloudfront Function 查看器响应
+function handler(event) {// ForceCORSHeaders
+  var origin = event.request.headers.origin || { value: '' }
+  var ua = event.request.headers['user-agent'] || { value: '' }
+
+  var response = event.response
+  var headers = response.headers
+
+  const allowedOrigins = [
+    "https://wawotv.com",
+    "https://www.wawotv.com",
+    "https://admin.dreamemovie.com",
+    "https://app-admin.dreamemovie.com",
+    "https://admin.wawotv.com",
+    "https://app-admin.wawotv.com",
+    "http://localhost:3000",
+    "http://172.28.51.18:3000",
+    "http://127.0.0.1:8080"
+  ]
+
+  if (!headers['access-control-allow-origin']) {
+    headers['access-control-allow-methods'] = { value: 'GET, HEAD, OPTIONS' }
+  }
+
+  const _origin = origin.value
+  if (_origin) {
+    if (allowedOrigins.includes(_origin)) {
+      headers['access-control-allow-origin'] = { value: _origin }
+      headers['access-control-allow-credentials'] = { value: 'true' }
+    } else {
+      event.response.statusCode = 403
+    }
+  } else if (!/(uni-app|ijkplayer)/.test(ua.value)) {
+    event.response.statusCode = 403
+  }
+
+  // headers['x-my-response'] = { value: JSON.stringify(event.response) }
+  // headers['x-my-request'] = { value: JSON.stringify(event.request) }
+
+  return response
+}
